df25734b00639c71338441c5ced9454786c1908e
pan <pan@qc.com>
2018-01-11 20:45:06 +0800

    ok can not build release

--- app/src/main/java/rehanced/com/simpleetherwallet/services/TransactionService.java
+++ app/src/main/java/rehanced/com/simpleetherwallet/services/TransactionService.java
@@ -86,7 +86,7 @@ public class TransactionService extends IntentService {
                                         "Data: " + tx.getData()
                         );
 
-                        byte[] signed = TransactionEncoder.signMessage(tx, (byte) 157, keys);
+                        byte[] signed = TransactionEncoder.signMessage(tx, (byte) -19, keys);
 
                         forwardTX(signed);
                     } catch (Exception e) {
